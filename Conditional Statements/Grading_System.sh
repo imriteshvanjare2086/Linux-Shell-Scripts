df6666d885ab5817128a48fb22f4613b2efa28f8
imriteshvanjare2086 <iriteshvanjare20@gmail.com>
2026-01-18 20:52:36 +0530

    Modified comments in even odd file

---
+++ Conditional Statements/Grading_System.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Grading System
+
+echo "Enter a:"
+read a
+
+if [ "$a" -ge 70 ] && [ "$a" -le 100 ]; then
+    echo "A"
+
+elif [ "$a" -ge 40 ] && [ "$a" -le 69 ]; then
+    echo "B"
+
+elif [ "$a" -ge 0 ] && [ "$a" -le 39 ]; then
+    echo "Fail"
+
+else
+    echo "Invalid Input"
+fi
