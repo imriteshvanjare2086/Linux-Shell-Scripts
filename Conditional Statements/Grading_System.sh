ebc553c9359dfbb4b3cff10023ed480461536cca
imriteshvanjare2086 <iriteshvanjare20@gmail.com>
2026-01-18 20:58:17 +0530

    Modified Grading system file

--- Conditional Statements/Grading_System.sh
+++ Conditional Statements/Grading_System.sh
@@ -4,15 +4,19 @@
 echo "Enter a:"
 read a
 
-if [ "$a" -ge 70 ] && [ "$a" -le 100 ]; then
+if [ "$a" -ge 70 ] && [ "$a" -le 100 ]; 
+then
     echo "A"
 
-elif [ "$a" -ge 40 ] && [ "$a" -le 69 ]; then
+elif [ "$a" -ge 40 ] && [ "$a" -le 69 ]; 
+then
     echo "B"
 
-elif [ "$a" -ge 0 ] && [ "$a" -le 39 ]; then
+elif [ "$a" -ge 0 ] && [ "$a" -le 39 ]; 
+then
     echo "Fail"
 
 else
     echo "Invalid Input"
+    
 fi
